2e5a818ed904562e53cd3cf13331760fe509198c
serka <serka@OZBEY>
2023-06-19 17:20:53 +0300

    Proje dosyası ekle.

---
+++ RealHomeProject.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RealHomeProject.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
