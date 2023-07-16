ae7222e7cde52f3dbfc082cf8fbafd7ebdd226ca
Serkan Özbey Kurucu <kurucu741@gmail.com>
2023-07-16 18:46:42 +0300

    added by Admin Area and admin template

--- RealHomeProject.WebUI/Program.cs
+++ RealHomeProject.WebUI/Program.cs
@@ -75,5 +75,11 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
 app.Run();
