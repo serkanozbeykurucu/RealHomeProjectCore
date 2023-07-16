5afc5ccfb63d59f56e1ed0830526f0d112e95125
Serkan Özbey Kurucu <kurucu741@gmail.com>
2023-07-16 19:23:18 +0300

    added by admin area and Admin Dashboard Controller

---
+++ RealHomeProject.WebUI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealHomeProject.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]/[action]/")]
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
